3ad064dea9e93c0c904575c93e94b01daa2775c0
Sabrina Aguirre Funes <sabrinaaguirrefunes@gmail.com>
2023-09-16 21:43:48 -0300

    completada modificacion en ruta GET de products

---
+++ segundaPreEntrega/README.md
@@ -0,0 +1,40 @@
+# desafiosBackendAguirreFunes
+
+#### Segunda Pre Entrega (carpeta _"segundaPreEntrega"_)
+
+En base a las entregas anteriores, se modifico el metodo GET para que recibiera por query params, de manera opcional, un _limit_, una _page_, un _sort_, y un _query_ (el mismo puede ser para filtrar por _category_ o _status_).
+
+De acuerdo a lo indicado en clase, no se desarrallaron las nuevas vistas ni los _prevLink_ y _nextLink_ para esta entrega.
+
+#### Vistas
+
+- ruta _/static_ -> muestra, utilizando unicamente handlebars, un vista con el listado de todos los productos
+- ruta _/realtimeproducts_ -> muestra, utilizando en conjunto handlebars y websocket, una vista con:
+  - un formulario para agregar productos
+  - un formulario para eliminar un producto a partir de un id
+  - un listado de todos los productos (el cual se actualiza automaticamente cada vez que se agrega o elimina un producto)
+- ruta _/chat_ -> muestra, utilizando en conjunto handlebars y websocket, una vista con:
+  - un formulario para enviar mensajes
+  - un listado de todos los mensajes en la base de datos (el cual se actualiza automaticamente cada vez que se agrega un mensaje)
+
+#### Rutas
+
+- ruta GET _/api/products_ -> al no recibir un query, leerá el archivo de productos y devolverá la totalidad del mismo como un array en formato string.
+- ruta GET _/api/products?limit=x&page=x&sort=x&category=x_ o _/api/products?limit=x&page=x&sort=x&status=x_-> podra recibir por query un límite _x_ que definirá el límite de resultados a mostrar, una page _x_ que determinara que pagina mostrara, un sort _x_ que ordenara los productos por precio segun se indique, una category o un status _x_ para filtrar los resultados que se desean ver. Todos estos parametros recibidos por query son _opcionales_. En caso de no indicarse, por default el limit sera 10, el page sera 1, el sort no realizara ordenamiento, y, si no se indica ni category ni status, no se filtrara y se realizara una busqueda general. Se leerá el archivo de productos y devolverá mediante el metodo paginate la informacion filtrada, paginada y ordenada segun los parametros solicitados.
+- ruta GET _/api/products/:pid_ -> recibirá por params el pid (id de producto) y devolverá sólo el producto solicitado como un array con un único objeto en formato string.
+- ruta POST _/api/products_ -> agregara un nuevo producto con los parametros recibidos del body (se recibira en formato json los datos del producto).
+- ruta PUT _/api/products/:pid_ -> actualizara los campos del producto del id indicado con los datos recibidos del body (los mismos se recibiran en formato json desde el body)
+- ruta DELETE _/api/products/:pid_ -> eliminara el producto del id indicado.
+
+- ruta POST _/api/carts/_ -> creara un nuevo carrito con un id y un array vacio para los productos
+- ruta GET _/api/carts/:cid_ -> devolvera los datos del carrito del id indicado por params.
+- ruta POST _/api/carts/:cid/product/:pid_ -> agregara al carrito del id indicado en params, el producto del id indicado en params. Se guardara en el carrito solo el id del producto y una variable de cantidad. Si el producto ya existiera en ese carrito, se sumara 1 en la variable cantidad.
+
+Se utilizaron las rutas para crear y modificar los productos y carritos mediante POSTMAN.
+
+##### Pasos para ejecutar el servidor
+
+- En primer instancia, se deberá crear, a la altura del archivo .gitignore, un archivo .env donde debera copiarse la informacion proporcionada por el chat de la plataforma de coderhouse.
+- Luego, en la terminal, se deberá entrar en la carpeta del desafío con el comando _"cd segundaPreEntrega"._
+- Una vez dentro de la carpeta se instalarán las dependencias con el comando: _"npm install"_
+- Finalmente se podrá poner en marcha el servidor con el comando: _"npm run dev"_
---
+++ segundaPreEntrega/src/index.js
@@ -0,0 +1,161 @@
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import { engine } from "express-handlebars";
+import { Server } from "socket.io";
+import { __dirname } from "./path.js";
+import path from "path";
+import { productsModel } from "./models/products.models.js";
+import { messagesModel } from "./models/messages.models.js";
+import prodsRouter from "./routes/products.routes.js";
+import cartsRouter from "./routes/carts.routes.js";
+
+const PORT = 8080;
+const app = express();
+
+//Mongoose
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connected"))
+  .catch(() => console.log("Error en conexion a BDD"));
+
+//Config
+
+const serverExpress = app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
+});
+
+//Middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.resolve(__dirname, "./views"));
+app.use("/static", express.static(path.join(__dirname, "/public")));
+app.use("/realtimeproducts", express.static(path.join(__dirname, "/public")));
+app.use("/chat", express.static(path.join(__dirname, "/public")));
+
+//Server with express
+const io = new Server(serverExpress);
+
+io.on("connection", (socket) => {
+  console.log("Servidor Socket.io conectado");
+  socket.on("newProd", async (prod) => {
+    const { title, description, code, price, stock, category } = prod;
+
+    try {
+      const prod = await productsModel.create({
+        title,
+        description,
+        code,
+        price,
+        stock,
+        category,
+      });
+      console.log("Product added successfully");
+    } catch (error) {
+      console.error("Error creating product");
+    }
+
+    let prods = await productsModel.find();
+    socket.emit("prodsData", prods);
+  });
+
+  socket.on("deleteProd", async (idDelete) => {
+    const idToDelete = idDelete;
+
+    const prod = await productsModel.findById(idToDelete);
+
+    if (prod) {
+      await productsModel.findByIdAndDelete(idToDelete);
+      console.log("Product removed seccessfully");
+    } else {
+      console.error("Product not found");
+    }
+
+    let prods = await productsModel.find();
+    socket.emit("prodsData", prods);
+  });
+
+  socket.on("initialProds", async () => {
+    try {
+      let prods = await productsModel.find();
+      socket.emit("prodsData", prods);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  socket.on("newMessage", async (newMessage) => {
+    const { email, message } = newMessage;
+
+    try {
+      const createMessage = await messagesModel.create({
+        email,
+        message,
+      });
+      console.log("Message added successfully");
+    } catch (error) {
+      console.error("Error creating message");
+    }
+
+    let messages = await messagesModel.find();
+    socket.emit("messageData", messages);
+  });
+
+  socket.on("initialMessages", async () => {
+    try {
+      let messages = await messagesModel.find();
+      socket.emit("messageData", messages);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+});
+
+let prodsStatic = [];
+const getProds = async () => {
+  try {
+    prodsStatic = await productsModel.find().lean();
+  } catch (error) {
+    console.error(error);
+  }
+};
+getProds();
+
+//Routes
+
+app.get("/", (req, res) => {
+  res.send("Bienvenido al desafio de la clase 8.");
+});
+
+app.use("/api/products", prodsRouter);
+app.use("/api/carts", cartsRouter);
+
+app.get("/static", (req, res) => {
+  res.render("home", {
+    css: "style.css",
+    title: "Home",
+    js: "home.js",
+    listProds: prodsStatic,
+  });
+});
+
+app.get("/realtimeproducts", (req, res) => {
+  res.render("realTimeProducts", {
+    css: "style.css",
+    title: "Products",
+    js: "realTimeProducts.js",
+  });
+});
+
+app.get("/chat", (req, res) => {
+  res.render("chat", {
+    css: "style.css",
+    title: "Chat",
+    js: "chat.js",
+  });
+});
